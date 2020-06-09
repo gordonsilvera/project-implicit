735f8448a84661b76ac1c51004f00ef4eb4bc2c1
“gordonsilvera” <gordon.silvera@gmail.com>
2020-06-09 17:34:53 +0000

    updates to spss_loader.ipynb; need schema yaml

--- src/configs.py
+++ src/configs.py
@@ -1,3 +1,3 @@
-CREDENTIALS_FILEPATH = '/home/jovyan/project-implicit/src/algomosaic-nyc-5e8f766c6725.json'
+CREDENTIALS_FILEPATH = '/home/jovyan/project-implicit/src/algomosaic-nyc-5711132fa0c3.json'
 CONFIGS_FILEPATH = '/home/jovyan/project-implicit/src/configs.py'
 SOURCE_CODE_FILEPATH = '/home/jovyan/project-implicit/src'
